81df9c5da97d8619718b583dce90f6af5bfceb7c
albertoesco <alumno.538326@ies.azarquiel.es>
2023-10-09 16:50:36 +0200

    Last commit

--- P2_2_Figuras.cs
+++ P2_2_Figuras.cs
@@ -10,32 +10,33 @@ public class P2_2_Figuras
 {
   static void Main(string[] args)
   {
-    List<Figura> lista_figuras = new List<Figura>();
+    double suma = 0;
 
-    // Crear instancias de diferentes figuras y agregarlas a la lista
+    //llamadas a las clases
+    List<Figura> figuras = new List<Figura>();
     Rectangulo r1 = new Rectangulo(3.9, 3, Color.FromArgb(255, 50, 20, 200));
+    figuras.Add(r1);
+    Rectangulo r2 = new Rectangulo(5.2, 2, Color.FromArgb(255, 150, 50, 31));
+    figuras.Add(r2);
     Circulo c1 = new Circulo(4, Color.FromArgb(255, 30, 120, 10));
+    figuras.Add(c1);
+    Circulo c2 = new Circulo(2, Color.FromArgb(255, 120, 200, 10));
+    figuras.Add(c2);
     TrianguloEquilatero t1 = new TrianguloEquilatero(3, Color.FromArgb(255, 230, 20, 30));
-
-    lista_figuras.Add(r1);
-    lista_figuras.Add(c1);
-    lista_figuras.Add(t1);
-
-    // Crear más instancias y agregarlas a la lista
-    Rectangulo r2 = new Rectangulo(4.5, 2, Color.FromArgb(255, 50, 20, 200));
-    Circulo c2 = new Circulo(3, Color.FromArgb(255, 30, 120, 10));
-    TrianguloEquilatero t2 = new TrianguloEquilatero(5, Color.FromArgb(255, 230, 20, 30));
-
-    lista_figuras.Add(r2);
-    lista_figuras.Add(c2);
-    lista_figuras.Add(t2);
+    figuras.Add(t1);
+    t1 = new TrianguloEquilatero(5, Color.FromArgb(255, 60, 150, 5));
+    figuras.Add(t1);
 
 
-    foreach (var figura in lista_figuras)
+    //bucle para recorrar la lista
+    foreach (Figura figura in figuras)
     {
-      Console.WriteLine(figura.ToString() + ", Área: " + figura.GetArea());
-    }
-  }
+      Console.WriteLine(figura.ToString());
+      Console.WriteLine();
+      suma += figura.GetArea();
 
+    }
+    Console.WriteLine($"La suma total del area es: {suma}");
+  }
 
 }
