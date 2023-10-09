95ceaefe4e264031a2a895c9917dc4ccceac865e
albertoesco <alumno.538326@ies.azarquiel.es>
2023-10-09 16:44:09 +0200

    Third commit

--- P2_2_Figuras.cs
+++ P2_2_Figuras.cs
@@ -10,21 +10,31 @@ public class P2_2_Figuras
 {
   static void Main(string[] args)
   {
+    List<Figura> lista_figuras = new List<Figura>();
+
+    // Crear instancias de diferentes figuras y agregarlas a la lista
     Rectangulo r1 = new Rectangulo(3.9, 3, Color.FromArgb(255, 50, 20, 200));
     Circulo c1 = new Circulo(4, Color.FromArgb(255, 30, 120, 10));
     TrianguloEquilatero t1 = new TrianguloEquilatero(3, Color.FromArgb(255, 230, 20, 30));
 
-    Console.WriteLine("Rectángulo: " + r1.ToString() + ", Área: " + r1.GetArea());
-    Console.WriteLine("Círculo: " + c1.ToString() + ", Área: " + c1.GetArea());
-    Console.WriteLine("Triángulo Equilátero: " + t1.ToString() + ", Área: " + t1.GetArea());
+    lista_figuras.Add(r1);
+    lista_figuras.Add(c1);
+    lista_figuras.Add(t1);
 
+    // Crear más instancias y agregarlas a la lista
     Rectangulo r2 = new Rectangulo(4.5, 2, Color.FromArgb(255, 50, 20, 200));
     Circulo c2 = new Circulo(3, Color.FromArgb(255, 30, 120, 10));
     TrianguloEquilatero t2 = new TrianguloEquilatero(5, Color.FromArgb(255, 230, 20, 30));
 
-    Console.WriteLine("Rectángulo: " + r2.ToString() + ", Área: " + r2.GetArea());
-    Console.WriteLine("Círculo: " + c2.ToString() + ", Área: " + c2.GetArea());
-    Console.WriteLine("Triángulo Equilátero: " + t2.ToString() + ", Área: " + t2.GetArea());
+    lista_figuras.Add(r2);
+    lista_figuras.Add(c2);
+    lista_figuras.Add(t2);
+
+
+    foreach (var figura in lista_figuras)
+    {
+      Console.WriteLine(figura.ToString() + ", Área: " + figura.GetArea());
+    }
   }
 
 
